ea13373b87b1b108e99580df8571f687d0c83785
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-11-05 22:59:30 +0900

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-env_name="pytorch39"
+env_name="torch39"
 conda create -y -n ${env_name} python=3.9 -c conda-forge
 conda install -y -n ${env_name} -c pytorch pytorch::pytorch torchvision torchaudio
-conda install -y -n ${env_name} -c conda-forge scikit-learn lightning jupyter umap-learn matplotlib seaborn pandas
+conda install -y -n ${env_name} -c conda-forge scikit-learn lightning jupyter umap-learn matplotlib seaborn pandas rdkit tqdm optuna graphviz
 
-$HOME/miniforge3/envs/${env_name}/bin/pip install torch_geometric
-$HOME/miniforge3/envs/${env_name}/bin/pip install --no-deps japanize-matplotlib
+$HOME/miniforge3/envs/${env_name}/bin/pip install --no-deps japanize-matplotlib torch_geometric torchviz
