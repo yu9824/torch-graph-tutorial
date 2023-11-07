01d312575f622c3d1689dafa66aa6fd2947d793a
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-11-08 07:05:12 +0900

    Update torch_utils to use regression

--- src/torch_utils.py
+++ src/torch_utils.py
@@ -6,64 +6,69 @@ LICENSE:
 - https://github.com/makaishi2/pythonlibs/blob/main/LICENSE
 """  # noqa: E501
 
-# 共通関数定義
-# Apache License Version 2.0
-# https://www.apache.org/licenses/LICENSE-2.0.html
-
-# ReadMe
-README = "Common Library for PyTorch\nAuthor: M. Akaishi"
+from typing import TypeVar, Union
+from collections.abc import Callable, Iterable
+import pkgutil
 
 import numpy as np
 import matplotlib.pyplot as plt
-import japanize_matplotlib
+import japanize_matplotlib  # noqa: F401
 
 import torch
-from torch import tensor
-import torch.nn as nn
-import torch.optim as optim
-from torchviz import make_dot
+import torch.nn
+import torch.utils.data
+import torch_geometric.data
 
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
-import torchvision.datasets as datasets
-from tqdm.notebook import tqdm
+# for type hint
+T = TypeVar("T")
+DataBatch = Union[torch_geometric.data.Batch, torch_geometric.data.Data]
+DataLoader = Union[
+    torch_geometric.data.DataLoader,
+    Iterable[DataBatch],
+]
+LossFn = Union[
+    torch.nn.modules.loss._Loss,
+    Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+]
 
 
 # 損失関数値計算用
-def eval_loss(loader, device, net, criterion):
+def eval_loss(
+    loader: DataLoader,
+    net: torch.nn.Module,
+    criterion: LossFn,
+    device: torch.device = torch.device("cpu"),
+) -> torch.Tensor:
     # DataLoaderから最初の1セットを取得する
-    for images, labels in loader:
-        break
+    data = next(iter(loader))
 
     # デバイスの割り当て
-    inputs = images.to(device)
-    labels = labels.to(device)
+    data = data.to(device)
 
     # 予測値の計算
-    outputs = net(inputs)
+    y_pred = net(data)
 
     #  損失値の計算
-    loss = criterion(outputs, labels)
+    loss = criterion(y_pred, data.y)
 
     return loss
 
 
 # 学習用関数
 def fit(
-    net,
-    optimizer,
-    criterion,
-    num_epochs,
-    train_loader,
-    test_loader,
-    device,
-    history,
-):
+    net: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    criterion: LossFn,
+    train_loader: DataLoader,
+    test_loader: DataLoader,
+    device: torch.device = torch.device("cpu"),
+    num_epochs: int = 100,
+    history: np.ndarray = np.empty((0, 3)),
+    ipynb: bool = False,
+) -> np.ndarray:
     base_epochs = len(history)
 
     for epoch in range(base_epochs, num_epochs + base_epochs):
-        # 1エポックあたりの正解数(精度計算用)
-        n_train_acc, n_val_acc = 0, 0
         # 1エポックあたりの累積損失(平均化前)
         train_loss, val_loss = 0, 0
         # 1エポックあたりのデータ累積件数
@@ -72,24 +77,34 @@ def fit(
         # 訓練フェーズ
         net.train()
 
-        for inputs, labels in tqdm(train_loader):
+        if any(_module.name == "tqdm" for _module in pkgutil.iter_modules()):
+            if ipynb:
+                from tqdm.notebook import tqdm
+            else:
+                from tqdm import tqdm
+        else:
+
+            def tqdm(x: T) -> T:
+                return x
+
+        # for inputs, labels in tqdm(train_loader):
+        for data_train in tqdm(train_loader):
             # 1バッチあたりのデータ件数
-            train_batch_size = len(labels)
+            train_batch_size = len(data_train)
             # 1エポックあたりのデータ累積件数
             n_train += train_batch_size
 
             # GPUヘ転送
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+            data_train = data_train.to(device)
 
             # 勾配の初期化
             optimizer.zero_grad()
 
             # 予測計算
-            outputs = net(inputs)
+            y_pred_train = net(data_train)
 
             # 損失計算
-            loss = criterion(outputs, labels)
+            loss = criterion(y_pred_train, data_train.y)
 
             # 勾配計算
             loss.backward()
@@ -97,64 +112,52 @@ def fit(
             # パラメータ修正
             optimizer.step()
 
-            # 予測ラベル導出
-            predicted = torch.max(outputs, 1)[1]
-
             # 平均前の損失と正解数の計算
             # lossは平均計算が行われているので平均前の損失に戻して加算
             train_loss += loss.item() * train_batch_size
-            n_train_acc += (predicted == labels).sum().item()
 
         # 予測フェーズ
         net.eval()
 
-        for inputs_test, labels_test in test_loader:
+        for data_test in test_loader:
             # 1バッチあたりのデータ件数
-            test_batch_size = len(labels_test)
+            test_batch_size = len(data_test)
             # 1エポックあたりのデータ累積件数
             n_test += test_batch_size
 
             # GPUヘ転送
-            inputs_test = inputs_test.to(device)
-            labels_test = labels_test.to(device)
+            data_test = data_test.to(device)
 
             # 予測計算
-            outputs_test = net(inputs_test)
+            y_pred_test = net(data_test)
 
             # 損失計算
-            loss_test = criterion(outputs_test, labels_test)
-
-            # 予測ラベル導出
-            predicted_test = torch.max(outputs_test, 1)[1]
+            loss_test = criterion(y_pred_test, data_test.y)
 
             #  平均前の損失と正解数の計算
             # lossは平均計算が行われているので平均前の損失に戻して加算
             val_loss += loss_test.item() * test_batch_size
-            n_val_acc += (predicted_test == labels_test).sum().item()
 
-        # 精度計算
-        train_acc = n_train_acc / n_train
-        val_acc = n_val_acc / n_test
         # 損失計算
         avg_train_loss = train_loss / n_train
         avg_val_loss = val_loss / n_test
         # 結果表示
         print(
-            f"Epoch [{(epoch+1)}/{num_epochs+base_epochs}], loss: {avg_train_loss:.5f} acc: {train_acc:.5f} val_loss: {avg_val_loss:.5f}, val_acc: {val_acc:.5f}"
+            f"Epoch [{(epoch+1)}/{num_epochs+base_epochs}], "
+            f"loss: {avg_train_loss:.5f} "
+            f"val_loss: {avg_val_loss:.5f}, "
         )
         # 記録
-        item = np.array(
-            [epoch + 1, avg_train_loss, train_acc, avg_val_loss, val_acc]
-        )
+        item = np.array([epoch + 1, avg_train_loss, avg_val_loss])
         history = np.vstack((history, item))
     return history
 
 
 # 学習ログ解析
-def evaluate_history(history):
+def evaluate_history(history: np.ndarray):
     # 損失と精度の確認
-    print(f"初期状態: 損失: {history[0,3]:.5f} 精度: {history[0,4]:.5f}")
-    print(f"最終状態: 損失: {history[-1,3]:.5f} 精度: {history[-1,4]:.5f}")
+    print(f"初期状態: 損失: {history[0,2]:.5f}")
+    print(f"最終状態: 損失: {history[-1,2]:.5f}")
 
     num_epochs = len(history)
     if num_epochs < 10:
@@ -165,7 +168,7 @@ def evaluate_history(history):
     # 学習曲線の表示 (損失)
     plt.figure(figsize=(9, 8))
     plt.plot(history[:, 0], history[:, 1], "b", label="訓練")
-    plt.plot(history[:, 0], history[:, 3], "k", label="検証")
+    plt.plot(history[:, 0], history[:, 2], "k", label="検証")
     plt.xticks(np.arange(0, num_epochs + 1, unit))
     plt.xlabel("繰り返し回数")
     plt.ylabel("損失")
@@ -173,70 +176,17 @@ def evaluate_history(history):
     plt.legend()
     plt.show()
 
-    # 学習曲線の表示 (精度)
-    plt.figure(figsize=(9, 8))
-    plt.plot(history[:, 0], history[:, 2], "b", label="訓練")
-    plt.plot(history[:, 0], history[:, 4], "k", label="検証")
-    plt.xticks(np.arange(0, num_epochs + 1, unit))
-    plt.xlabel("繰り返し回数")
-    plt.ylabel("精度")
-    plt.title("学習曲線(精度)")
-    plt.legend()
-    plt.show()
 
+def torch_seed(seed: int = 123) -> None:
+    """PyTorch乱数固定用
 
-# イメージとラベル表示
-def show_images_labels(loader, classes, net, device):
-    # DataLoaderから最初の1セットを取得する
-    for images, labels in loader:
-        break
-    # 表示数は50個とバッチサイズのうち小さい方
-    n_size = min(len(images), 50)
-
-    if net is not None:
-        # デバイスの割り当て
-        inputs = images.to(device)
-        labels = labels.to(device)
-
-        # 予測計算
-        outputs = net(inputs)
-        predicted = torch.max(outputs, 1)[1]
-        # images = images.to('cpu')
-
-    # 最初のn_size個の表示
-    plt.figure(figsize=(20, 15))
-    for i in range(n_size):
-        ax = plt.subplot(5, 10, i + 1)
-        label_name = classes[labels[i]]
-        # netがNoneでない場合は、予測結果もタイトルに表示する
-        if net is not None:
-            predicted_name = classes[predicted[i]]
-            # 正解かどうかで色分けをする
-            if label_name == predicted_name:
-                c = "k"
-            else:
-                c = "b"
-            ax.set_title(label_name + ":" + predicted_name, c=c, fontsize=20)
-        # netがNoneの場合は、正解ラベルのみ表示
-        else:
-            ax.set_title(label_name, fontsize=20)
-        # TensorをNumPyに変換
-        image_np = images[i].numpy().copy()
-        # 軸の順番変更 (channel, row, column) -> (row, column, channel)
-        img = np.transpose(image_np, (1, 2, 0))
-        # 値の範囲を[-1, 1] -> [0, 1]に戻す
-        img = (img + 1) / 2
-        # 結果表示
-        plt.imshow(img)
-        ax.set_axis_off()
-    plt.show()
-
-
-# PyTorch乱数固定用
-
-
-def torch_seed(seed=123):
+    Parameters
+    ----------
+    seed : int, optional
+        , by default 123
+    """
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.mps.manual_seed(seed)  # for ARM
     torch.backends.cudnn.deterministic = True
     torch.use_deterministic_algorithms = True
